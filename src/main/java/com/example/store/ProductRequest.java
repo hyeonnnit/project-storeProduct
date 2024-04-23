cb16c9bfa14400554d51c7132947b89652c4e2bb
lhyeonnit <lhyeonnit@gmail.com>
2024-04-23 11:25:16 +0900

    ProductRequest SaveDTO

--- src/main/java/com/example/store/ProductRequest.java
+++ src/main/java/com/example/store/ProductRequest.java
@@ -1,4 +1,26 @@
 package com.example.store;
-public class ProductRequest {
 
+import com.example.store._core.common.PicSaveUtil;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ProductRequest {
+    @Data
+    public static class SaveDTO {
+        private String name;
+        private int price;
+        private int qty;
+        private MultipartFile pic;
+
+        public Product toEntity() {
+            String picPath = PicSaveUtil.save(pic);
+            return Product.builder()
+                    .pic(picPath)
+                    .name(name)
+                    .price(price)
+                    .qty(qty)
+                    .build();
+
+        }
+    }
 }
