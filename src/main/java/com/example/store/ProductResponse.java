36b129f540710da85df6af15f5a1602181d97576
lhyeonnit <lhyeonnit@gmail.com>
2024-04-23 10:46:09 +0900

    ProductResponse ListDTO

--- src/main/java/com/example/store/ProductResponse.java
+++ src/main/java/com/example/store/ProductResponse.java
@@ -1,5 +1,21 @@
 package com.example.store;
 
 
+import lombok.Data;
+
 public class ProductResponse {
+    @Data
+    public static class ListDTO{
+        private int id;
+        private String name;
+        private int price;
+        private int qty;
+
+        public ListDTO(Product product){
+            this.id = product.getId();
+            this.name = product.getName();
+            this.price = product.getPrice();
+            this.qty = product.getQty();
+        }
+    }
 }
