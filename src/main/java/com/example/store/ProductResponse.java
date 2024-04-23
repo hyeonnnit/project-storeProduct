135ef00f1cdeb7e604027f3428c7c3db73429dc7
lhyeonnit <lhyeonnit@gmail.com>
2024-04-23 11:09:59 +0900

    이미지 필드 추가 ë및 ProductResponse DetailDTO

--- src/main/java/com/example/store/ProductResponse.java
+++ src/main/java/com/example/store/ProductResponse.java
@@ -5,6 +5,23 @@ import lombok.Data;
 
 public class ProductResponse {
     @Data
+    public static class DetailDTO{
+        private int id;
+        private String name;
+        private int price ;
+        private int qty;
+        private String pic;
+
+        public DetailDTO(Product product){
+            this.id = product.getId();
+            this.name = product.getName();
+            this.price = product.getPrice();
+            this.qty = product.getQty();
+            this.pic = product.getPic();
+        }
+    }
+
+    @Data
     public static class ListDTO{
         private int id;
         private String name;
